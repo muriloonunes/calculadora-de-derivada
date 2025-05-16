d6b718b773e0c9f0b43f5df771877ce140cdf060
murilo <57834900+muriloonunes@users.noreply.github.com>
2025-05-16 20:09:10 -0300

    mudanças no front end

--- src/main/java/mpbhd/calculadoradederivada/controller/CalculadoraController.java
+++ src/main/java/mpbhd/calculadoradederivada/controller/CalculadoraController.java
@@ -1,12 +1,16 @@
 package mpbhd.calculadoradederivada.controller;
 
-import javafx.application.Platform;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
 import org.matheclipse.core.eval.ExprEvaluator;
 import org.matheclipse.core.interfaces.IExpr;
 
@@ -21,7 +25,7 @@ public class CalculadoraController {
     private Button derivadaButton, integralButton;
 
     @FXML
-    private TextField entradaField, saidaField;
+    private TextField derivadaField, integralField;
 
     @FXML
     private TabPane tabPainel;
@@ -34,6 +38,7 @@ public class CalculadoraController {
 
     @FXML
     protected void onDerivadaButtonClick() {
+        animarCliqueBotao(Color.CYAN, derivadaButton);
         System.out.println("Derivada clicado");
         ExprEvaluator util = new ExprEvaluator();
         IExpr result = util.evaluate("D(x^2 + 3*x, x)");  // Derivada de x^2 + 3x
@@ -42,10 +47,26 @@ public class CalculadoraController {
 
     @FXML
     protected void onIntegralButtonClick() {
+        animarCliqueBotao(Color.GOLDENROD, integralButton);
         System.out.println("Integral clicado");
     }
 
     private void alterarVisibilidade(Label label) {
         label.setVisible(!label.isVisible());
+    }
+
+    private void animarCliqueBotao(Color cor, Button botao) {
+        DropShadow glow = new DropShadow();
+        glow.setColor(cor);
+        glow.setRadius(0);
+        glow.setInput(null);
+        botao.setEffect(glow);
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.ZERO, new KeyValue(glow.radiusProperty(), 0)),
+                new KeyFrame(Duration.seconds(0.1), new KeyValue(glow.radiusProperty(), 30)),
+                new KeyFrame(Duration.seconds(0.55), new KeyValue(glow.radiusProperty(), 0))
+        );
+        timeline.setCycleCount(1);
+        timeline.play();
     }
 }
--- src/main/java/mpbhd/calculadoradederivada/view/CalculadoraApplication.java
+++ src/main/java/mpbhd/calculadoradederivada/view/CalculadoraApplication.java
@@ -3,9 +3,9 @@ package mpbhd.calculadoradederivada.view;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import javafx.scene.image.Image;
 import mpbhd.calculadoradederivada.controller.CalculadoraController;
 
 import java.io.IOException;
@@ -14,13 +14,13 @@ import java.util.Objects;
 public class CalculadoraApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-Font.loadFont(Objects.requireNonNull(CalculadoraApplication.class.getResource("/font/Roboto-Regular.ttf")).toExternalForm(), 10);
+        Font.loadFont(Objects.requireNonNull(CalculadoraApplication.class.getResource("/font/Roboto-Regular.ttf")).toExternalForm(), 10);
 
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/mpbhd/calculadoradederivada/calculadora.fxml"));
-        CalculadoraController calculadoraController = fxmlLoader.getController();
-
         Scene scene = new Scene(fxmlLoader.load(), 740, 560);
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/styles/styles.css")).toExternalForm());
+
+        CalculadoraController calculadoraController = fxmlLoader.getController();
 
         stage.getIcons().add(new Image(Objects.requireNonNull(CalculadoraApplication.class.getResourceAsStream("/images/icon.png"))));
         stage.setTitle("Calculadora de Derivadas");
